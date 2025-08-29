cbba22ad5ac1b0217b5900e2a2235141a68bc5a5
Suchir Kavi <suchirkavi@gmail.com>
2025-08-29 16:05:54 -0700

    two_ways: actually fix a_then_b
    B needs to drop immediately after polling ready.

--- src/two_ways/a_then_b.rs
+++ src/two_ways/a_then_b.rs
@@ -10,52 +10,169 @@ pub mod auto {
 pub mod manual {
     use core::{
         future::Future,
-        pin::{pin, Pin},
-        task::Poll,
+        mem::MaybeUninit,
+        pin::Pin,
+        task::{ready, Context, Poll},
     };
-    use std::task::ready;
 
     use pin_project::pin_project;
 
-    pub async fn a_then_b<A: Future<Output = ()>, B: Future<Output = ()>>(a: A, b: B) {
-        DoAThenB {
-            state: State::PendingA(a),
-            b,
-        }
-        .await
-    }
-
-    #[pin_project(project = StateProj)]
-    enum State<A> {
-        PendingA(#[pin] A),
-        ReadyA,
-    }
-
-    #[pin_project]
-    struct DoAThenB<A, B> {
-        #[pin]
-        state: State<A>,
-        #[pin]
+    pub fn a_then_b<A: Future<Output = ()>, B: Future<Output = ()>>(
+        a: A,
         b: B,
+    ) -> impl Future<Output = ()> {
+        DoAThenB::new(a, b)
+    }
+
+    #[pin_project(project = DoAThenBProj)]
+    enum DoAThenB<A, B> {
+        DoingA {
+            #[pin]
+            a: A,
+            b: MaybeUninit<B>,
+        },
+        DoingB {
+            #[pin]
+            b: B,
+        },
+        Done,
+    }
+
+    impl<A, B> DoAThenB<A, B> {
+        pub fn new(a: A, b: B) -> Self {
+            Self::DoingA {
+                a,
+                b: MaybeUninit::new(b),
+            }
+        }
     }
 
     impl<A: Future<Output = ()>, B: Future<Output = ()>> Future for DoAThenB<A, B> {
         type Output = ();
 
-        fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-            let mut this = self.project();
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
             loop {
-                match this.state.as_mut().project() {
-                    StateProj::PendingA(a) => {
+                let this = self.as_mut().project();
+                match this {
+                    DoAThenBProj::DoingA { a, b } => {
                         ready!(a.poll(cx));
-                        this.state.set(State::ReadyA);
+                        // SAFETY: b is initialized in `DoAThenB::new`. We read
+                        // it only once then drop the `MaybeUninit` by setting
+                        // the state to `DoingB`.
+                        let b = unsafe { MaybeUninit::assume_init_read(b) };
+                        self.set(DoAThenB::DoingB { b });
                     }
-                    StateProj::ReadyA => {
-                        ready!(this.b.poll(cx));
+                    DoAThenBProj::DoingB { b } => {
+                        ready!(b.poll(cx));
+                        self.set(DoAThenB::Done);
                         break Poll::Ready(());
+                    }
+                    DoAThenBProj::Done => {
+                        panic!("`async fn` resumed after completion");
                     }
                 }
             }
+        }
+    }
+}
+
+/// This version attempts to use tail calls to avoid looping in `poll`.
+pub mod manual_opt {
+    use core::{
+        future::Future,
+        hint::unreachable_unchecked,
+        mem::MaybeUninit,
+        pin::Pin,
+        task::{ready, Context, Poll},
+    };
+
+    use pin_project::pin_project;
+
+    pub fn a_then_b<A: Future<Output = ()>, B: Future<Output = ()>>(
+        a: A,
+        b: B,
+    ) -> impl Future<Output = ()> {
+        DoAThenB::new(a, b)
+    }
+
+    #[pin_project(project = DoAThenBProj)]
+    enum DoAThenB<A, B> {
+        DoingA {
+            #[pin]
+            a: A,
+            b: MaybeUninit<B>,
+        },
+        DoingB {
+            a: MaybeUninit<A>,
+            #[pin]
+            b: B,
+        },
+        Done,
+    }
+
+    impl<A, B> DoAThenB<A, B> {
+        pub fn new(a: A, b: B) -> Self {
+            Self::DoingA {
+                a,
+                b: MaybeUninit::new(b),
+            }
+        }
+    }
+
+    impl<A: Future<Output = ()>, B: Future<Output = ()>> Future for DoAThenB<A, B> {
+        type Output = ();
+
+        fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            match &*self {
+                // SAFETY: `self` is in the `DoingA` state.
+                DoAThenB::DoingA { .. } => unsafe { self.doing_a(cx) },
+                // SAFETY: `self` is in the `DoingB` state.
+                DoAThenB::DoingB { .. } => unsafe { self.doing_b(cx) },
+                DoAThenB::Done => panic!("`async fn` resumed after completion"),
+            }
+        }
+    }
+
+    impl<A: Future<Output = ()>, B: Future<Output = ()>> DoAThenB<A, B> {
+        /// # Safety
+        ///
+        /// `self` must be in the `DoingA` state.
+        unsafe fn doing_a(mut self: Pin<&mut DoAThenB<A, B>>, cx: &mut Context<'_>) -> Poll<()> {
+            // scope so temps are dropped before the tail call
+            {
+                let this = self.as_mut().project();
+                let DoAThenBProj::DoingA { a, b } = this else {
+                    // SAFETY: caller must ensure `self` is in the `DoingA` state.
+                    unsafe { unreachable_unchecked() }
+                };
+
+                ready!(a.poll(cx));
+                let a = MaybeUninit::uninit();
+                // SAFETY: b is initialized in `DoAThenB::new`. We read
+                // it only once then drop the `MaybeUninit` by setting
+                // the state to `DoingB`.
+                let b = unsafe { MaybeUninit::assume_init_read(b) };
+                self.set(DoAThenB::DoingB { a, b });
+            }
+
+            // tail call hopefully
+            // SAFETY: we've just set `self` to the `DoingB` state.
+            unsafe { self.doing_b(cx) }
+        }
+
+        /// # Safety
+        ///
+        /// `self` must be in the `DoingB` state.
+        unsafe fn doing_b(mut self: Pin<&mut DoAThenB<A, B>>, cx: &mut Context<'_>) -> Poll<()> {
+            let this = self.as_mut().project();
+            let DoAThenBProj::DoingB { a: _, b } = this else {
+                // SAFETY: caller must ensure `self` is in the `DoingB` state.
+                unsafe { unreachable_unchecked() }
+            };
+
+            ready!(b.poll(cx));
+            self.set(DoAThenB::Done);
+            Poll::Ready(())
         }
     }
 }
